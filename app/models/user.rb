0468e85ff882b36205cc1775aca785dbc58010f7
usama-imran89 <usama.imran@devsinc.com>
2022-05-31 18:38:08 +0500

    UserModel:Add user using devise gem

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  enum role: [:standard,:admin]
+  after_initialize do
+    if self.new_record?
+      self.role ||= :standard
+    end
+  end
+end
