b678eaf5d9d157229de4320b2175859ba90c1c7c
usama-imran89 <usama.imran@devsinc.com>
2022-06-02 16:23:18 +0500

    Subject: add user column in user devise
    Subject: add user update and edit functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[ :fname, :lname, :email, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[ :fname, :lname,:dname, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys:[ :fname, :lname,:dname, :email, :password, :current_password])
+
   end
 end
